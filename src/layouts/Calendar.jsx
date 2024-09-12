721a65cabc9758b11e1d32a92447a1fe6a1a6887
TomJamesYap28 <tjyap28@gmail.com>
2024-09-12 15:49:08 +0800

    up

--- src/layouts/Calendar.jsx
+++ src/layouts/Calendar.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import listPlugin from '@fullcalendar/list';
 import { CalendarAddModal, CalendarEditModal } from '../assets/hooks/Modals.jsx';
 
 function Calendar({ isAdmin }) {
+  const calendarRef = useRef(null);
+  const [selectedDate, setSelectedDate] = useState('');
+  const [editEventData, setEditEventData] = useState(null);
+
+  const handleDateClick = (info) => {
+    if (isAdmin) {
+      clearForm('taskForm');
+      setSelectedDate(info.dateStr); // Set the selected date for the modal
+      window.$('#addtaskModal').modal('show'); // Show the add event modal
+    }
+  };
+
+  const handleEventClick = (info) => {
+    if (isAdmin) {
+      const event = info.event;
+      setEditEventData({
+        id: event.id,
+        date: event.startStr,
+        title: event.title,
+        description: event.extendedProps.description,
+      }); // Set the event data for editing
+      window.$('#editTaskModal').modal('show'); // Show the edit event modal
+    }
+  };
+
+  const clearForm = (formId) => {
+    const form = document.getElementById(formId);
+    if (form) {
+      form.reset();
+      const hiddenInputs = form.querySelectorAll('input[type="hidden"]');
+      hiddenInputs.forEach((input) => {
+        input.value = '';
+      });
+    }
+  };
+
   return (
     <div className="calendar">
       <FullCalendar
@@ -23,8 +59,8 @@ function Calendar({ isAdmin }) {
             right: 'dayGridMonth,listMonth',
         }}
         dateClick={handleDateClick}
-        events={fetchEvents}
-        eventDidMount={handleEventMount}
+        // events={fetchEvents}
+        // eventDidMount={handleEventMount}
       />
 
       <CalendarAddModal selectedDate={selectedDate} />
